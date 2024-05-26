918601704a0833ac61d31b4771f76173a03bbc60
aslayon <137384998+aslayon@users.noreply.github.com>
2024-05-26 23:12:07 +0900

    Add files via upload

---
+++ Paint/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Paint.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_PaintTYPE                   130
+#define ID_Pen                          32774
+#define ID_Eraser                       32775
+#define ID_Rect                         32776
+#define ID_Circle                       32777
+#define ID_BUTTON32778                  32778
+#define ID_BUTTON32779                  32779
+#define ID_SAVE_AS_VECTOR               32780
+#define ID_OPEN_AS_VEC                  32781
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32782
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
